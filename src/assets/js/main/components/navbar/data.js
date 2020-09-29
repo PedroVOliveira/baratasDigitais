97b69096a3e1b1b7a895fb6903462b4842ef617f
Pedro <pedrovoliveiracontato@gmail.com>
2020-09-29 20:41:28 -0300

    modify javascript

--- src/assets/js/main/components/navbar/data.js
+++ src/assets/js/main/components/navbar/data.js
@@ -2,4 +2,5 @@ let navbarLiElement = {
     navbar:document.querySelector('header nav'),
     ulElement: document.querySelector('header nav .container ul'),
     items: ['Home','Recursos','A Baratas','Preços','Contato'],
+    ids:['#home','#features','#about','#prices','#contact']
 }
--- src/assets/js/main/components/navbar/index.js
+++ src/assets/js/main/components/navbar/index.js
@@ -1,8 +1,8 @@
-let {navbar,items,ulElement} = navbarLiElement;
+let {navbar,items,ulElement,ids} = navbarLiElement;
 
 ulElement.innerHTML = items.map((item,index) =>{
     return (
-        `<li><a href="${items[index]}">${item}</a></li>`
+        `<li><a href="${ids[index]}">${item}</a></li>`
     )
 }).join('');
 
